298dc2792fb106e1a9753f17f1553a1528cd2127
Matheus351 <matheusniklaus@gmail.com>
2023-07-04 15:54:20 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Example from './assets/components/Dialog'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Example/>
+      </div>
+    </>
+  )
+}
+
+export default App
