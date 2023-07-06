3755746388cde1c62d72cefc792582ac264fa226
Gustvo98 <gustavocontas@gmail.com>
2023-07-06 11:23:09 -0300

    adicionando listbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Example from "./assets/components/Dialog";
 import Tabs from "./assets/components/Tab";
+import Listbox from "./assets/components/Listbox";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <div>
           <Example />
           <Tabs/>
+          <Listbox/>
       </div>
 
     </>
